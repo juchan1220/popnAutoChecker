8920844fa864486e26b04bd4b025fd2dba0b3647
nulldori <develop@nulldori.tech>
2021-02-11 13:55:54 +0900

    해명리들스 대응 및 jQuery 제거

--- assets/js/49.js
+++ assets/js/49.js
@@ -1,66 +1,75 @@
-// javascript:var s=document.createElement("script");s.src="https://popn.nulldori.tk/48.js";s.type="text/javascript";document.getElementsByTagName("body")[0].appendChild(s);
+// javascript:var s=document.createElement("script");s.src="https://popn.nulldori.tech/48.js";s.type="text/javascript";document.getElementsByTagName("body")[0].appendChild(s);
 
-let medalData = {};
+let TARGET_LEVEL = 49
+let BASE_URL = "https://p.eagate.573.jp/game/popn/riddles/playdata/mu_lv.html"
 
-let virtualDocument = document.implementation.createHTMLDocument('virtual');
+let medalData = {}
 
-let script = document.createElement('script');
-script.src = '//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName("head")[0].append(script);
+function initDocument (html) {
+    let doc = document.implementation.createHTMLDocument()
+    doc.documentElement.innerHTML = html
 
-function getPage (currentPage, lastPage)
-{
-    if(currentPage === lastPage){
-        let form = document.createElement('form');
-        let input = document.createElement('input');
-        input.setAttribute('type', 'hidden');
-        input.setAttribute('value', encodeURIComponent(JSON.stringify(medalData)));
-        input.setAttribute('name', 'data');
-
-        form.setAttribute('method', 'post');
-        form.setAttribute('action', "https://popn.nulldori.tech/49.php");
-
-        form.appendChild(input);
-        document.body.appendChild(form);
-        form.submit();
-
-        return ;
-    }
-
-    $.get("https://p.eagate.573.jp/game/popn/peace/p/playdata/mu_lv.html?page=" + currentPage + "&level=49").then(data => {
-        $(data, virtualDocument).find("ul.mu_list_table li").each(function(){
-            if($(this).find("div.th_music_lv").length === 0){
-                let title = $(this).find("a")[0].textContent;
-                let medal_imgsrc = $(this).find("div.col_ex_lv img")[0].getAttribute("src").split('/');
-                let rank_imgsrc = $(this).find("div.col_ex_lv img")[1].getAttribute("src").split('/');
-                let medal = medal_imgsrc[medal_imgsrc.length - 1];
-                let rank = rank_imgsrc[rank_imgsrc.length - 1];
-                let score = $(this).find("div.col_ex_lv")[0].textContent;
-
-                medalData[title] = {medal, rank, score};
-            }
-        });
-
-
-        getPage(currentPage + 1, lastPage)
-    });
+    return doc
 }
 
-checkReady = (callback) => {
-    if (window.jQuery){
-        callback(jQuery);
-    }
-    else{
-        window.setTimeout(() => checkReady(callback), 500);
-    }
-};
+async function getPage(page) {
+    let param = new URLSearchParams()
 
-checkReady(function($) {
-    $(function() {
-        $.get("https://p.eagate.573.jp/game/popn/peace/p/playdata/mu_lv.html?page=0&level=49").then(data => {
-            let totalPage = $(data, virtualDocument).find("ul.mu_list_table")[0].lastElementChild.children.length - 2;
-            getPage(0, totalPage);
-        });
-    });
-});
+    param.append("page", page)
+    param.append("level", TARGET_LEVEL)
+
+    return await fetch(`${BASE_URL}?${param.toString()}`)
+        .then(res => res.arrayBuffer())
+        .then(buffer => {
+            return new TextDecoder("shift-jis").decode(buffer)
+        })
+        .then(text => initDocument(text))
+}
+
+async function crawlPage(page) {
+    let virtualDocument = await getPage(page)
+    let table = virtualDocument.querySelector('ul.mu_list_table')
+
+    let rows = Array.from(table.children).slice(1)
+
+    for(let row of rows) {
+        let title = row.querySelector('a').textContent;
+        let medal_imgsrc = row.querySelectorAll("div.col_ex_lv img")[0].getAttribute("src").split('/');
+        let rank_imgsrc = row.querySelectorAll("div.col_ex_lv img")[1].getAttribute("src").split('/');
+        let medal = medal_imgsrc[medal_imgsrc.length - 1];
+        let rank = rank_imgsrc[rank_imgsrc.length - 1];
+        let score = row.querySelector("div.col_ex_lv").textContent;
+
+        medalData[title] = {medal, rank, score};
+    }
+}
+
+function sendData() {
+    let form = document.createElement('form')
+
+    let input = document.createElement('input')
+    input.setAttribute('type', 'hidden')
+    input.setAttribute('value', encodeURIComponent(JSON.stringify(medalData)))
+    input.setAttribute('name', 'data')
+
+    form.setAttribute('method', 'post')
+    form.setAttribute('action', `https://popn.nulldori.tech/${TARGET_LEVEL}.php`)
+
+    form.appendChild(input)
+    document.body.appendChild(form)
+    form.submit()
+}
+
+async function init() {
+    let document = await getPage(0)
+    let table = document.querySelector('ul.mu_list_table')
+    let totalPage = table.nextElementSibling.children.length - 2
+
+    for (let p = 0; p < totalPage; p++) {
+        await crawlPage(p)
+    }
+
+    sendData()
+}
+
+init()
